cb3fb6e76a7b72811e285db12dbc2ec3e33c4595
NGabbai2006 <ngabbai.pro@gmail.com>
2025-11-01 12:40:25 +0100

    modif de la page2 pour rendre le lien fonctionnable et la page 3 fini

--- NavBug/script.js
+++ NavBug/script.js
@@ -1,15 +1,16 @@
+
 function verifier() {
   const reponse = document.getElementById("reponse").value.toLowerCase(); // creer une variable pour recuperer la reponse et  
   // tolowercase met en minuscule la réponse
   const terminal = document.getElementById("terminal"); // creer une variable pour le terminal qui me permetra d'afficher des messages
 
-  if (reponse === "serveur") { // condition si la reponse est bien serveur
-    terminal.innerHTML += "<p>Accès autorisé. Redirection...</p>"; // affiche le message dnas la div terminal et le <p> permet de faire un saut de ligne
+  if (reponse === "1992") { // condition si la reponse est bien serveur
+    terminal.innerHTML += "<p>Mot-clé correct! fuite possible!</p>"; // affiche le message dnas la div terminal et le <p> permet de faire un saut de ligne
     // le innerHTML permet d'ajouter  ou modif le contenu html pour ce code c'est dans une div
       setTimeout(() => { // att 2s avant d'aller sur la pageweb suivante
-      window.location.href = "../NavBug/page3.html";
+      window.location.href = "../page4/page4.html";
     }, 2000); 
-  } else { // sinon 
-    terminal.innerHTML += "<p>Mot-clé incorrect. Intrusion détectée.</p>";
+  } else { 
+    terminal.innerHTML += "<p>Mot-clé incorrect. le site ne tiendera plus longtemps</p>";
   }
 }
